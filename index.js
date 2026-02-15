cc6001b67c31a8960e7595457371580957f9f057
nareshindgithub <75830644+nareshindgithub@users.noreply.github.com>
2026-02-15 18:39:15 +0530

    Change server listening port from 81 to 80

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.write('Welcome to DevOps Training'); //write a response to the client
   res.end(); //end the response
-}).listen(81); //the server object listens on port 80
+}).listen(80); //the server object listens on port 80
